1346b76bdf837aca702df12d582fb8b683767302
Nicky Christensen <nicky@wearewebstars.dk>
2019-05-31 08:54:39 +0200

    added to -gitignore

---
+++ src/firebase/FirebaseConfigExample.js
@@ -0,0 +1,12 @@
+/**
+ * Config file for our firebase instance
+ */
+
+export default {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_AUTH_DOMAIN_KEY',
+  databaseURL: 'https://YOURAPP.firebaseio.com',
+  projectId: 'PROJECTID',
+  storageBucket: 'APPNAME.appspot.com',
+  messagingSenderId: 'SENDERID'
+};
